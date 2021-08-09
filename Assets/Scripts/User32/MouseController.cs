ce43a1cb7e3204bcd070fc282cd385144f33dbbb
Wagner Colombo Dias <wagco@outlook.com>
2021-08-09 11:12:45 -0300

    Adiciona controladores para o Mouse e para as janelas

---
+++ Assets/Scripts/User32/MouseController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using UnityEngine;
+
+namespace LoLRunes.User32
+{
+    public static class MouseController
+    {
+        [Flags]
+        private enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+
+        [DllImport("user32.dll")]
+        private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int x, int y);
+
+        public static void LeftClick(Point p)
+        {
+            SetCursorPos(p.X, p.Y);
+            mouse_event((int)(MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            mouse_event((int)(MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+        }
+
+        public static void LightClick(Point p)
+        {
+            SetCursorPos(p.X, p.Y);
+            mouse_event((int)(MouseEventFlags.RIGHTDOWN), 0, 0, 0, 0);
+            mouse_event((int)(MouseEventFlags.RIGHTUP), 0, 0, 0, 0);
+        }
+
+        public static void DoubleRightClick(Point p)
+        {
+            SetCursorPos(p.X, p.Y);
+            mouse_event((int)(MouseEventFlags.RIGHTDOWN), 0, 0, 0, 0);
+            mouse_event((int)(MouseEventFlags.RIGHTUP), 0, 0, 0, 0);
+            mouse_event((int)(MouseEventFlags.RIGHTDOWN), 0, 0, 0, 0);
+            mouse_event((int)(MouseEventFlags.RIGHTUP), 0, 0, 0, 0);
+        }
+
+        public static void HolDownLeft(Point p, int tempo)
+        {
+            SetCursorPos(p.X, p.Y);
+            mouse_event((int)(MouseEventFlags.LEFTDOWN), 0, 0, 0, 0);
+            Thread.Sleep(tempo);
+            mouse_event((int)(MouseEventFlags.LEFTUP), 0, 0, 0, 0);
+        }
+
+        public static void HolDownRight(Point p, int tempo)
+        {
+            SetCursorPos(p.X, p.Y);
+            mouse_event((int)(MouseEventFlags.RIGHTDOWN), 0, 0, 0, 0);
+            Thread.Sleep(tempo);
+            mouse_event((int)(MouseEventFlags.RIGHTUP), 0, 0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/User32/WindowController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace LoLRunes.User32
+{
+    public static class WindowController
+    {
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string className, string windowName);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public static void SetForegroundWindow(string windowName)
+        {
+            int hWnd = FindWindow(null, windowName);
+
+            SetForegroundWindow((IntPtr)hWnd);
+        }
+    }
+}

9bb2be9ad4b1fe5319967aa587a701d4a08a6105
Wagner Colombo Dias <wagco@outlook.com>
2021-08-09 14:20:21 -0300

    Realiza o teste das funcionalidadas do namespace User32

--- Assets/Scripts/Unity/ApplicationManager.cs
+++ Assets/Scripts/Unity/ApplicationManager.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Gma.UserActivityMonitor;
-using System.Windows.Forms;
 using TMPro;
 using System.Runtime.InteropServices;
 using System;
-using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using Gma.UserActivityMonitor;
+using LoLRunes.User32;
 
 public class ApplicationManager : MonoBehaviour
 {
@@ -17,22 +16,13 @@ public class ApplicationManager : MonoBehaviour
 
     [SerializeField] private TMP_Text text;
 
-    private GlobalEventProvider globalEventProvider;    
-
     private List<Point> positions;
     private int count;
 
     // Start is called before the first frame update
     void Start()
     {
-        globalEventProvider = new GlobalEventProvider();
-        
-        
-        IntPtr a = GetModuleHandle(Process.GetCurrentProcess().MainModule.ModuleName);
 
-        Process[] process = Process.GetProcesses().Where(p => p.ProcessName.Contains("League")).ToArray();
-
-        MouseEventExtArgs e = new MouseEventExtArgs(MouseButtons.Left, 0, 0, 0, 0);
     }
 
     // Update is called once per frame
@@ -51,7 +41,7 @@ public class ApplicationManager : MonoBehaviour
         }
         else
         {
-            globalEventProvider.MouseDown -= HookManager_MouseMove;
+            MSWindowsEventManager.instance.Unsubscribe_MouseDown(HookManager_MouseMove);
         }
 
         text.text = string.Format("x={0:0000}; y={1:0000}", e.X, e.Y); 
@@ -59,33 +49,22 @@ public class ApplicationManager : MonoBehaviour
 
     public void ButtonFindWindow()
     {
-        var clickClass = new ClickClass();
-
-        int hWnd = ClickClass.FindWindow(null, "League of Legends");
-        //ClickClass.MaximizeWindow((IntPtr)hWnd);
-        ClickClass.SetForegroundWindow((IntPtr)hWnd);
-        ClickClass.SetCursorPos(1250, 540);
-        //
-        clickClass.leftClick(new Point());
+        WindowController.SetForegroundWindow("League of Legends");
     }
 
     public void ButtonPerformClicks()
     {
         var clickClass = new ClickClass();
 
-        int hWnd = ClickClass.FindWindow(null, "League of Legends");
-        ClickClass.SetForegroundWindow((IntPtr)hWnd);
+        WindowController.SetForegroundWindow("League of Legends");
 
         foreach (Point point in positions)
-        {
-            ClickClass.SetCursorPos(point.X, point.Y);
-            clickClass.leftClick(new Point());
-        }
+            MouseController.LeftClick(point);
     }
 
     public void ButtonUnsubscribe()
     {
-        globalEventProvider.MouseDown -= HookManager_MouseMove;
+        MSWindowsEventManager.instance.Unsubscribe_MouseDown(HookManager_MouseMove);
     }
 
     public void ButtonSynchronize()
@@ -93,11 +72,11 @@ public class ApplicationManager : MonoBehaviour
         count = 0;
         positions = new List<Point>() { Point.Empty, Point.Empty, Point.Empty, Point.Empty };
 
-        globalEventProvider.MouseDown += HookManager_MouseMove;
+        MSWindowsEventManager.instance.Subscribe_MouseDown(HookManager_MouseMove);
     }
 
     private void OnDestroy()
     {
-        globalEventProvider.MouseDown -= HookManager_MouseMove;
+        MSWindowsEventManager.instance.UnsubscribeAll();
     }
 }

7df71ad33e2d724d93060e498de9f8c6bb205ebb
Wagner Colombo Dias <wagco@outlook.com>
2021-08-10 19:09:21 -0300

    Implementa scriptable object para gerenciar as posições das runas em casa resolução

---
+++ Assets/Scripts/Scriptable Objects/RunePositionConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ResolutionRunePositionConfig_xx", menuName = "Scriptable Objects/Positioning/ResolutionRunePositionConfig")]
+public class ResolutionRunePositionConfig : ScriptableObject
+{
+    [SerializeField] private int resolutionX;
+    [SerializeField] private int resolutionY;
+    [SerializeField] private int windowX;
+    [SerializeField] private int windowY;
+    [SerializeField] private string positions;
+}

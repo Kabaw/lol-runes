7f70ebc6945c5caac5d4931f4d22e3302c47ad07
Wagner Colombo Dias <wagco@outlook.com>
2021-08-11 01:13:11 -0300

    Implement TransformUntil

---
+++ Assets/Scripts/Utils/Unity Components/TransformUntil.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LoLRunes.Utils.UnityComponents
+{
+    public static class TransformUtility
+    {
+        public static Transform FindParentByTag(Transform transform, string tag)
+        {
+            if (!transform)
+                return null;
+
+            Transform parentTransform = transform.parent;
+
+            while (parentTransform)
+            {
+                if(parentTransform.CompareTag(tag))
+                    return parentTransform;
+                else
+                    parentTransform = parentTransform.parent;
+            }
+
+            return null;
+        }
+    }
+}

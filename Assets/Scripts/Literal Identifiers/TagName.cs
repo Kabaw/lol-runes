3d79a49faf79f45f5c83809fde80bbac183d9138
Wagner Colombo Dias <wagco@outlook.com>
2021-08-17 09:05:43 -0300

    Desenvolvendo o comportamento de seleção do path e do side path

--- Assets/Scripts/Literal Identifiers/TagName.cs
+++ Assets/Scripts/Literal Identifiers/TagName.cs
@@ -5,5 +5,10 @@ namespace LoLRunes.LiteralIdentifiers
     public class TagName
     {
         public static readonly string PathRoot = "PathRoot";
+        public static readonly string Precision = "Precision";
+        public static readonly string Domination = "Domination";
+        public static readonly string Sorcery = "Sorcery";
+        public static readonly string Resolve = "Resolve";
+        public static readonly string Inspiration = "Inspiration";
     }
 }
---
+++ Assets/Scripts/View/UI/PathComp.cs
@@ -0,0 +1,30 @@
+﻿using LoLRunes.Enumerators;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LoLRunes.View.UI
+{
+    public class PathComp : MonoBehaviour
+    {
+        [SerializeField] private PathTypeEnum pathType;        
+        [SerializeField] private List<GameObject> keyStones;
+        [SerializeField] private List<GameObject> runeSets;
+
+        public void ButtonClick(RuneButton runeButton)
+        {
+            ActivateByTag(keyStones, runeButton.tag);
+            ActivateByTag(runeSets, runeButton.tag);
+        }
+
+        private void ActivateByTag(List<GameObject> gameObjects, string tag)
+        {
+            foreach (GameObject go in gameObjects)
+            {
+                if (go.CompareTag(tag))
+                    go.SetActive(true);
+                else
+                    go.SetActive(false);
+            }
+        }
+    }
+}

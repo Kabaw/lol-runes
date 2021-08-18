ce39bc8a75824e6a22b943d70f190f2414dd0d20
Wagner Colombo Dias <wagco@outlook.com>
2021-08-18 13:07:59 -0300

    Implementa o conceito de rune groups para ser utilizado como atributo do RuneType

---
+++ Assets/Scripts/Attributes/RuneGroupAttribute.cs
@@ -0,0 +1,18 @@
+﻿using LoLRunes.Enumerators;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LoLRunes.CustumAttributes
+{
+    public class RuneGroupAttribute : Attribute
+    {
+        public RuneGroupEnum RuneGroup { get; }
+
+        public RuneGroupAttribute(RuneGroupEnum runeGroup)
+        {
+            RuneGroup = runeGroup;
+        }
+    }
+}
---
+++ Assets/Scripts/Enums/RuneGroupEnum.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LoLRunes.Enumerators
+{
+    public enum RuneGroupEnum
+    {
+        PATH,
+        KEYSTONE,
+        RUNE_01,
+        RUNE_02,
+        RUNE_03,
+        SHARDS_ATTACK,
+        SHARDS_FLEX,
+        SHARDS_DEFENCE
+    }
+}

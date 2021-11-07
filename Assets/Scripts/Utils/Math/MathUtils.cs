f1793bcbc5cd19d0b4dc9cb919fedc71314829b7
Wagner Colombo Dias <wagco@outlook.com>
2021-11-07 15:32:05 -0300

    Adiciona regra de três

---
+++ Assets/Scripts/Utils/Math/MathUtils.cs
@@ -0,0 +1,17 @@
+﻿namespace Assets.Scripts.Utils.Math
+{
+    public class MathUtils
+    {
+        /// <summary>
+        /// A is to B as C is to X
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <returns>X</returns>
+        public static float RuleOfThree(float a, float b, float c)
+        {
+            return (b * c) / a;
+        }
+    }
+}

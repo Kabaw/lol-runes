7c0584cb0cb3002d46d9e5ec8a25a034beb7ac57
Wagner Colombo Dias <wagco@outlook.com>
2022-11-18 11:06:49 -0300

    Desenvolvendo sistema de runa Lcu

---
+++ Assets/Scripts/Shared/Dtos/LcuRune.cs
@@ -0,0 +1,12 @@
+﻿using LoLRunes.Shared.Enums;
+using System;
+
+namespace Assets.Scripts.Shared.Dtos
+{
+    [Serializable]
+    public class LcuRune
+    {
+        public string id;
+        public RuneTypeEnum runeType;
+    }
+}
